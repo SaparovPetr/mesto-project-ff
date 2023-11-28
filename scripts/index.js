0f5a036928852558620cf9e7a03f704c35cf9ff2
Petr Saparov <saparov.petr@yandex.ru>
2023-11-28 18:31:07 +0300

    28-11-23

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,8 @@
 const cardTemplate = document.querySelector('#card-template').content;
 const content = document.querySelector('.content');
 const placesList = content.querySelector('.places__list');
-const cardsNamesOnly = initialCards.map(function (el) {
-  return el.name;
-});
-const cardsLinksOnly = initialCards.map(function (el) {
-  return el.link;
-});
 
-function addCard(placeLink, placeName, removing) {
+function createCard(placeLink, placeName, removing) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   cardElement.querySelector('.card__image').src = placeLink;
   cardElement.querySelector('.card__image').alt = placeName;
@@ -16,13 +10,20 @@ function addCard(placeLink, placeName, removing) {
   cardElement.querySelector('.card__delete-button').addEventListener('click', function() {
     removing(cardElement);
   });
-  placesList.append(cardElement);  
+  return cardElement;
 }
 
 function deleteCard(cardElement) {
   cardElement.remove();
 }
 
-for (let i = 0; i < 6; i++)  {
-  addCard(cardsLinksOnly[i], cardsNamesOnly[i], deleteCard);
+function renderCard(placeLink, placeName, removing) {
+  const renderedCardElement = createCard(placeLink, placeName, removing);
+  placesList.prepend(renderedCardElement);
 }
+
+initialCards.forEach(function(item) {
+  renderCard(item.link, item.name, deleteCard);
+});
+
+
